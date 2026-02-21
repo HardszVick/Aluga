4ea0ea7423f93a4bb700e1ef4c32518a0fedc6a9
antony-93 <user.antony.cechinel@gmail.com>
2026-02-21 14:05:33 -0300

    refac: Removed vitest setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
     plugins: [react(), tsconfigPaths()],
     test: {
         environment: 'happy-dom',
-        globals: true,
-        setupFiles: './vitest.setup.ts',
+        globals: true
     }
 })
